d55b0792dfccd8ebdaf33702ce35d0c5652bba99
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Pro.local>
2021-07-14 23:50:48 +0530

    added new features

--- JS/DigitalClock/script.js
+++ JS/DigitalClock/script.js
@@ -2,19 +2,89 @@
 
 let currentTime=(clockName)=>{
     let date = new Date()
-    let hours, mins, secs, day 
-    hours = date.getHours()
-    mins = date.getMinutes()
-    secs = date.getSeconds()
-    day = date.getDay()
-    let time = hours + ":" + mins + ":" + secs + `  ${clockName}  `+ day
+    let hours, mins, secs, day, time
+    hours = updateTime(date.getHours())
+    mins = updateTime(date.getMinutes())
+    secs = updateTime(date.getSeconds())
+    day = date.getDay() // 0-6
+    dayArr = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+    day = dayArr[day]
+
+    if(hours < 12 && hours!=0){
+        time = hours + ":" + mins + ":" + secs + `  ${clockName}  `+ day + " AM "
+    }
+    else if(hours > 12){
+        time = updateTime(hours-12) + ":" + mins + ":" + secs + `  ${clockName}  `+ day + " PM "
+    }
+    else if(hours == 12){
+        time = hours + ":" + mins + ":" + secs + `  ${clockName}  `+ day + " PM "
+    }
+    else if(hours == 0){
+        hours = 12
+        time = hours + ":" + mins + ":" + secs + `  ${clockName}  `+ day + " AM "
+    }
+    
+     
     document.getElementById("clock").innerHTML = time
+
 }
 // currentTime
+
 
 setInterval(()=>{
     currentTime("OurClock")
 },1000)
+
+function updateTime(k){
+  if(k < 10){
+    return ("0"+k)
+  }
+  return (k)
+  
+}
+
+
+// function convertDay(d){
+//     switch(d){
+//         case 0:
+//               return "Sunday"
+//         case 1:
+//               return "Monday"
+//         case 2:
+//               return "Tuesday"
+//         case 3:
+//               return "Wednesday"
+//         case 4:
+//               return "Thrushday"
+//         case 5:
+//               return "Friday"
+//         case 6:
+//               return "Saturday"
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // setInterval(function (){
 
